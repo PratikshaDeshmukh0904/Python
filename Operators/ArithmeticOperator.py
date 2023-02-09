2ccba11a863da6ee3e275669d42a9c3007ad8e13
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-02-09 09:20:26 -0800

    Operators

---
+++ Operators/ArithmeticOperator.py
@@ -0,0 +1,20 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=12
+>>> b=12
+>>> c=a+b
+>>> print("Addition of a and b is",c)
+Addition of a and b is 24
+>>> d=a-b
+>>> print("Substraction of a and b is",d)
+Substraction of a and b is 0
+>>> e=a/b
+>>> print("Multiplication of a and b is",e)
+Multiplication of a and b is 1.0
+>>> f=a*b
+>>> print("Multiplication of a and b is",f)
+Multiplication of a and b is 144
+>>> print("Division of a and b is",e)
+Division of a and b is 1.0
+>>> a%b
+0
