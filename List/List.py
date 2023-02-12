7b06b83ac08b708bf2adb6822ba49226a76a2973
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-02-12 07:57:47 -0800

    List and All functions related List

---
+++ List/List.py
@@ -0,0 +1,4 @@
+list =['physics','chemistry',1999,2000]
+print("list",list)
+list2=[1,-2,3+4j,4,5.55]
+print(list2)
---
+++ List/ListBuiltinFunction.py
@@ -0,0 +1,29 @@
+list1 = ['physics','chemistry','maths']
+print(list1)
+print("length=",len(list1))
+list2=list(range(5) )#create list of numbers between 0-4
+print(list2)
+print("length",len(list2))
+
+
+
+#max function
+list1,list2=['C++','Java','Python'],[456,700,200]
+print(list1,list2)
+print("MAx value element :",max(list1))
+print("Max value element :",max(list2))
+
+
+#min function
+print("min value element:",min(list1))
+print("min value element:",min(list2))
+
+#list() function
+aTuple = (123,'C++','java','python')
+list1 = list(aTuple)
+print("List elements :",list1)
+
+
+str="Hello World"
+list2=list(str)
+print("list elements :",list2)
