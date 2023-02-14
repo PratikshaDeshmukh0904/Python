3d8c8dab1f439b184b6041de78fa852eaaf7923b
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-02-14 02:42:33 -0800

    Control-Flow Statement

---
+++ Control-Flow/if_in_if.py
@@ -0,0 +1,12 @@
+m=int(input())
+
+if(m>=35 or m>50):
+    print("Pass")
+    if(m>50 or m >60):
+        print("Second Class")
+        if(m>61 or m>75):
+           print("First Class")
+           if(m>76 and m<=100):
+               print("Distinction")
+print("Program over")
+           
